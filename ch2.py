48b2afd2c3673c996a70f4456dba8cf503edf877
dopeamine <shantanu.rajenimbalkar@gmail.com>
2016-12-14 20:00:56 -0500

    stylistics - differentiating genres based on words they use

--- ch2.py
+++ ch2.py
@@ -1,5 +1,8 @@
+import nltk
 from nltk.corpus import gutenberg # project gutenberg text
 from nltk.corpus import webtext # webtext corpora - discussion forum, pirated of carribean, etc
+from nltk.corpus import nps_chat
+from nltk.corpus import brown
 
 print "text_name  |  num_of_chars  |  num_of_words  |  num_of_sentences  |  num_of_vocab_items  |  avg_word_len  |  avg_sent_len  |  lexical_diversity"
 for fileid in gutenberg.fileids():
@@ -14,3 +17,24 @@ for fileid in gutenberg.fileids():
 
 for fileid in webtext.fileids():
     print fileid
+
+brown.categories()
+brown.raw("cr09")
+
+#stylistics - systematic differences between genres
+# by use of modal verbs - [can could may might must will]
+news_text = brown.words(categories='news')
+hobbies_text = brown.words(categories='hobbies')
+news_text_fdist = nltk.FreqDist(w.lower() for w in news_text)
+hobbies_text_fdist = nltk.FreqDist(w.lower() for w in hobbies_text)
+modals = ['can','could','may','might','must','will']
+for m in modals:
+    print m,":",news_text_fdist[m],"  |  ",hobbies_text_fdist[m]
+
+event_words = ["who","what","when","where","why"]
+for m in event_words:
+    print m,":",news_text_fdist[m],"  |  ",hobbies_text_fdist[m]
+
+cfd = nltk.ConditionalFreqDist((genre,word)for genre in brown.categories() for word in brown.words(categories=genre))
+genres = ['news', 'religion', 'hobbies', 'science_fiction', 'romance', 'humor']
+cfd.tabulate(conditions=genres, samples=modals)
